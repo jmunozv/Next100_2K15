c183e8937f679fce7a3466bd4a7de9e8cbd88fc9
jmunozv <javier.munoz.vidal@gmail.com>
2015-04-24 20:02:00 +0200

    Updating roadCharacterizer with NonHotTrkE

--- tracks/roadCharacterizer/roadCharacterizer.cc
+++ tracks/roadCharacterizer/roadCharacterizer.cc
@@ -32,21 +32,19 @@ bool roadCharacterizer::initialize() {
   // Histogram: Number of Tracks
   gate::Centella::instance()->hman()->h1(this->alabel("NumTracks"),
                                          "Number of Tracks", 10, 0, 10);
+
   // Histogram: Energy of Hottest Track
   gate::Centella::instance()->hman()->h1(this->alabel("EnergyHotTrk"),
-                                         "Energy of Hottest Track", 50, 2.0, 2.5);
-  // Histogram: Main Path Voxels of Hottest Track
-  gate::Centella::instance()->hman()->h1(this->alabel("NumMPVoxelsHotTrk"),
-                                         "Number of Main Path Voxels of Hottest Track",
-                                         60, 0, 120);
-  // Histogram: Energy of Second Hottest Track
-  gate::Centella::instance()->hman()->h1(this->alabel("Energy2HotTrk"),
-                                         "Energy of 2nd Hottest Track", 60, 0.0, 0.6);
-  // Histogram: Energies of Hottest Track vs Second Hottest Track
-  gate::Centella::instance()->hman()->h2(this->alabel("EnergyHotVS2Hot"),
-                                         "Energy of Hottest Track vs 2nd Hottest Track",
-                                         50, 2.0, 2.5, 60, 0.0, 0.6);
-  gate::Centella::instance()->hman()->fetch(this->alabel("EnergyHotVS2Hot"))->SetOption("COLZ");
+                                         "Energy of Hottest Track", 75, 2.35, 2.5);
+
+  // Histogram: Energy of Hottest Track
+  gate::Centella::instance()->hman()->h1(this->alabel("EnergyNonHotTrk"),
+                                         "Energy of Tracks (Non Hottest)", 50, 0.0, 0.1);
+
+  // // Histogram: Main Path Voxels of Hottest Track
+  // gate::Centella::instance()->hman()->h1(this->alabel("NumMPVoxelsHotTrk"),
+  //                                        "Number of Main Path Voxels of Hottest Track",
+  //                                        60, 0, 120);
 
   return true;
 }
@@ -79,28 +77,20 @@ bool roadCharacterizer::execute(gate::Event& evt) {
   gate::Centella::instance()->hman()->fill(this->alabel("EnergyHotTrk"), maxEdep);
   _m.message("Hottest Track ID:", hTrackID, " -> Edep:", maxEdep, gate::DETAILED);
 
-  int numOrdHits = hTrack->fetch_ivstore("MainPathHits").size();
-  gate::Centella::instance()->hman()->fill(this->alabel("NumMPVoxelsHotTrk"), numOrdHits);
-  _m.message("Hottest Track ID:", hTrackID, " -> Num Hits in the Main Path:", numOrdHits, gate::DETAILED);
+  // int numOrdHits = hTrack->fetch_ivstore("MainPathHits").size();
+  // gate::Centella::instance()->hman()->fill(this->alabel("NumMPVoxelsHotTrk"), numOrdHits);
+  // _m.message("Hottest Track ID:", hTrackID, " -> Num Hits in the Main Path:", numOrdHits, gate::DETAILED);
 
 
-  // Dealing with the 2nd Hottest Track
+  // Dealing with the Non Hottest Tracks
   if (numTracks > 1) {
-    double maxEdep2 = 0.;
-    gate::Track* hTrack2;
     for (auto track: tracks) {
       double eDep = track->GetEnergy();
-      if ((eDep > maxEdep2) && (eDep != maxEdep)) {
-        maxEdep2 = eDep;
-        hTrack2 = track;
+      if (eDep != maxEdep) {
+        gate::Centella::instance()->hman()->fill(this->alabel("EnergyNonHotTrk"), eDep);
+        _m.message("Non Hottest Track ID:", track->GetID(), " -> Edep:", eDep, gate::DETAILED);
       }
     }
-    int hTrackID2 = hTrack2->GetID();
-    gate::Centella::instance()->hman()->fill(this->alabel("Energy2HotTrk"), maxEdep2);
-    _m.message("2nd Hottest Track ID:", hTrackID2, " -> Edep:", maxEdep2, gate::DETAILED);
-
-    gate::Centella::instance()->hman()->fill2d(this->alabel("EnergyHotVS2Hot"), maxEdep, maxEdep2);
-
   }
 
   return true;
