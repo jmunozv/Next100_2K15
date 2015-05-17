506e038bbf073e09d44aca87c2c42da974661376
jmunozv <javier.munoz.vidal@gmail.com>
2015-05-17 12:24:23 +0200

    TrackEProfiler created

---
+++ tracks/trackEProfiler/trackEProfiler.h
@@ -0,0 +1,37 @@
+#ifndef _trackEProfiler__
+#define _trackEProfiler__
+
+#include <GATE/Centella.h>
+
+class trackEProfiler : public gate::IAlgo {
+
+ public:
+  
+  //! default contructor
+  trackEProfiler(gate::VLEVEL=gate::NORMAL,
+	       std::string label="trackEProfilerInstance");
+  
+  //! constructor with store with input parameters 
+  trackEProfiler(const gate::ParamStore& gs,
+	       gate::VLEVEL=gate::NORMAL,
+	       std::string label="trackEProfilerInstance");
+  
+  //! destructor
+  virtual ~trackEProfiler(){};
+  
+  //! initialize algorithm
+  bool initialize();        
+  
+  //! execute algorithm: process current event
+  bool execute(gate::Event& evt);  
+  
+  //! finalize algorithm
+  bool finalize();          
+  
+ private:
+  
+  ClassDef(trackEProfiler,0)
+    
+};
+
+#endif
